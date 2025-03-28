30aeb7b2ad7f7a966edaebef09923e4ea94a82aa
Nikittty <josefinamedinaa@gmail.com>
2025-03-27 19:38:10 -0500

    fx

---
+++ src/scripts/config.js
@@ -0,0 +1,7 @@
+export const config = {
+  baseUrl: "https://around-api.es.tripleten-services.com/v1",
+  token: "b50eedfe-a517-4d1a-a039-95b29b2e3adf",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
